0f3c3d14b55ea7a788408b486a910d9fe0d10281
lin_yujiao <25938803@qq.com>
2021-12-16 17:01:55 +0800

    自定义表头组件查询优化

--- src/components/TableColumns/index.jsx
+++ src/components/TableColumns/index.jsx
@@ -7,12 +7,23 @@ function TableColumns(props) {
   const { defaultVal, records, ChangeSelectVal } = props;
   const [searchkey, setSearchKey] = useState('');
   const [searchrow, setSearchRow] = useState(undefined);
+  const [inputVal, setInputVal] = useState('');
+  const [indexColumn, setIndexColumn] = useState([]);
+
+  useEffect(() => {
+    const newArr = defaultVal.map((item) => {
+      return item.key
+    });
+    setIndexColumn(newArr)
+  }, []);
 
   const defaultValue = defaultVal.map((item) => {
     return item.key
   });
 
   const onChange = (val) => {
+    // const newDefVal = val.concat(defaultValue);
+    // const arr = Array.from(new Set(newDefVal))
     const newArr = records.filter((x) => val.some((item) => x.key === item));
     ChangeSelectVal(newArr)
   }
@@ -31,14 +42,21 @@ function TableColumns(props) {
   return (
     <>
       <InputGroup compact>
-        <Input onChange={e => setSearchKey(e.target.value)} placeholder="请输入" allowClear style={{ width: '50%' }} />
+        <Input
+          value={inputVal}
+          onChange={e => { setSearchKey(e.target.value); setInputVal(e.target.value) }}
+          placeholder="请输入"
+          allowClear
+          style={{ width: '50%' }}
+        />
         <Button type="primary" onClick={() => handleSearch()} >查询</Button>
-        <Button onClick={() => setSearchRow(undefined)} >重 置</Button>
+        <Button onClick={() => { setInputVal(''); setSearchRow(undefined); onChange(defaultValue) }} >重置</Button>
+        <Button onClick={() => { setInputVal(''); setSearchRow(undefined); onChange(indexColumn) }} >默认表头</Button>
       </InputGroup>
-      <Checkbox.Group onChange={onChange} defaultValue={defaultValue}>
+      <Checkbox.Group onChange={onChange} value={defaultValue}>
         {searchrow ? (<Row style={{ marginTop: 24 }}>
           {searchrow && searchrow.length > 0 && searchrow.map((obj) => (
-            <Col span={8} >
+            <Col span={searchrow.length > 2 ? 8 : 24} >
               <Checkbox key={obj.val} value={obj.key}>{obj.val}</Checkbox>
             </Col>
           ))}
