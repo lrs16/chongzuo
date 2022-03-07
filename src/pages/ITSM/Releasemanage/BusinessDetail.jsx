a504e4b8f230d88ebc314ea841f33fa342b2b5dc
lin_yujiao <25938803@qq.com>
2022-03-07 10:47:02 +0800

    发布管理清单筛选

--- src/pages/ITSM/Releasemanage/BusinessDetail.jsx
+++ src/pages/ITSM/Releasemanage/BusinessDetail.jsx
@@ -17,6 +17,7 @@ function BusinessDetail(props) {
   const [visible, setVisible] = useState(false);
   const [visibleQuality, setVisibleQuality] = useState(false);
   const [toVerify, setToVerify] = useState(false);
+  const [myList, setMyList] = useState(undefined);
   const { currenttab } = useContext(EditContext);
   const user = sessionStorage.getItem('userName');
 
@@ -53,38 +54,41 @@ function BusinessDetail(props) {
   }
 
   const handlecompleteverify = () => {
-    if (selectedRecords && selectedRecords.length > 0) {
-      const Arr = selectedRecords.filter(obj => obj.responsible !== user);
-      if (Arr && Arr.length && Arr.length > 0) {
-        setVisible(true);
-      } else {
-        submitVerify();
-      }
-    } else {
-      message.error('请选择数据...')
-    }
+    // if (selectedRecords && selectedRecords.length > 0) {
+    //   const Arr = selectedRecords.filter(obj => obj.responsible !== user);
+    //   if (Arr && Arr.length && Arr.length > 0) {
+    //     setVisible(true);
+    //   } else {
+    //     submitVerify();
+    //   }
+    // } else {
+    //   message.error('请选择数据...')
+    // }
+    setMyList(undefined)
   };
 
   const handlemycompleteverify = () => {
     const Arr = info.filter(obj => obj.responsible === user);
     if (Arr && Arr.length > 0) {
-      const newArr = Arr.map((item) => {
-        return item.id
-      });
-      completeVerify({ listIds: newArr.toString() }).then(res => {
-        if (res.code === 200) {
-          message.success(res.msg);
-          router.push({
-            pathname: runpath,
-            query: { pathpush: true },
-            state: { cache: false, closetabid: Id }
-          });
-        } else {
-          message.error(res.msg || '验证失败！')
-        };
-      })
+      // const newArr = Arr.map((item) => {
+      //   return item.id
+      // });
+      setMyList(Arr);
+      // completeVerify({ listIds: newArr.toString() }).then(res => {
+      //   if (res.code === 200) {
+      //     message.success(res.msg);
+      //     router.push({
+      //       pathname: runpath,
+      //       query: { pathpush: true },
+      //       state: { cache: false, closetabid: Id }
+      //     });
+      //   } else {
+      //     message.error(res.msg || '验证失败！')
+      //   };
+      // })
     } else {
-      message.info('清单中没有您负责的业务...')
+      message.info('清单中没有您负责的业务...');
+      setMyList([]);
     }
   };
 
@@ -96,6 +100,7 @@ function BusinessDetail(props) {
           todoCode: Id,
         },
       });
+      setMyList(undefined);
     }
   }, [Id]);
 
@@ -143,7 +148,7 @@ function BusinessDetail(props) {
             placement="leftTop"
           >
             <Button type="primary" style={{ marginRight: 8 }} onClick={() => handlecompleteverify()}>
-              验证
+              全部清单
             </Button>
           </Popconfirm>
         </>
@@ -170,7 +175,7 @@ function BusinessDetail(props) {
         <BusinessEditTable
           title='发布清单'
           type={pagetitle}
-          dataSource={info}
+          dataSource={myList || info}
           ChangeValue={(v) => {
             if (v.status) {
               completeVerify({ listIds: v.target.id }).then(res => {
