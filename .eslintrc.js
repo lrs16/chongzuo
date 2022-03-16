6e173f1079b2f836b58602d6955000372f732177
maxinjing <483551685@qq.com>
2022-03-16 08:14:36 +0800

    还原此文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,4 @@ module.exports = {
     ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION: true,
     page: true,
   },
-  plugins: ['react-hooks'],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
 };
